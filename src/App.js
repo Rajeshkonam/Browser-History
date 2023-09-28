3738224090bf1c83c5027e8f687d796e061ae5c6
rajesh <rajeshkonam701@gamail.com>
2023-09-28 12:03:36 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,163 @@
+import {Component} from 'react'
+
+import './App.css'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
+// Replace your code here
+class App extends Component {
+  state = {searchInput: '', historyList: initialHistoryList, isTrue: false}
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  btnClick = id => {
+    const {historyList} = this.state
+    const filterData = historyList.filter(j => j.id !== id)
+    this.setState({historyList: filterData})
+    if (historyList.length === 0) {
+      this.setState({historyList: filterData, isTrue: true})
+    } else {
+      this.setState({historyList: filterData})
+    }
+  }
+
+  render() {
+    const {searchInput, historyList} = this.state
+    let {isTrue} = this.state
+    const searchResult = historyList.filter(i =>
+      i.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    if (searchResult.length === 0) {
+      isTrue = true
+    }
+    return (
+      <div className="c1">
+        <div className="c2">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+            alt="app logo"
+          />
+          <div className="c3">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+              alt="search"
+            />
+            <input
+              type="search"
+              onChange={this.onChangeSearchInput}
+              value={searchInput}
+              className="c4"
+            />
+          </div>
+        </div>
+        {/* jkvvvbk */}
+        <div>
+          {!isTrue && (
+            <ul>
+              {searchResult.map(i => (
+                <li key={i.id} id={i.id} className="li">
+                  <p>{i.timeAccessed}</p>
+                  <img src={i.logoUrl} alt="domain logo" />
+                  <p>{i.title}</p>
+                  <p>{i.domainUrl}</p>
+                  <button
+                    type="button"
+                    onClick={() => this.btnClick(i.id)}
+                    data-testid="delete"
+                  >
+                    <img
+                      src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+                      alt="delete"
+                    />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+          {isTrue && (
+            <div className="aa">
+              <p>There is no history to show</p>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
